b8ae022212079426962189d57e5653909a5207a9
123-dani <78221119+123-dani@users.noreply.github.com>
2021-02-19 14:23:50 -0500

    Add files via upload

---
+++ parcial.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+#include<conio.h>
+#include <windows.h> 
+#include <time.h> 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string>
+
+main() 
+
+{
+   int opc,  i==0;
+         while(i==0){
+         	 cout<<" 1 papitas que valen 1.500 , 2 doritos 2.000 , chocolate 2.500 ";
+         	 cin>>opc;
+         	 if(opc==1){
+         	 	cout<<"deme un billete de 2.000 y le devuelvo 500 pesos";
+         	 	}
+         	 	
+         	 	Return 0;
+         	 	(
+         	 	
+				  else{
+
+				  cout<<"si no me das un billete de 2.000  deme cualquier otro valor de dinero y le resto los 1500";
+				  )
+				   int opc,  i==0;
+         while(i==0){
+         	 cout<<" 1 papitas que valen 1.500 , 2 doritos 2.000 , chocolate 2.500 ";
+         	 cin>>opc;
+         	 if(opc==2){
+         	 	cout<<"deme un billete de 2.000 y quedamos en paz;
+         	 	}
+         	 	
+         	 	Return 0;
+         	 	(
+         	 	
+				  else{
+
+				  cout<<"deme un billete mayor a 2.000 para poderte devorverte o quedar en paz ";
+				  )
+				  int opc,  i==0
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+				  
+}
